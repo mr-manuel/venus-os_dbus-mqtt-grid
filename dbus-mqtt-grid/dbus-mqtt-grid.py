2c75a13c5d51cef4ddfe1099f0a840a93d159d70
Manuel <mr-manuel@outlook.it>
2023-12-18 15:08:58 +0100

    added timeout on startup

--- dbus-mqtt-grid/dbus-mqtt-grid.py
+++ dbus-mqtt-grid/dbus-mqtt-grid.py
@@ -366,7 +366,7 @@ class DbusMqttGridService:
         self._dbusservice.add_path("/ProductId", 0xFFFF)
         self._dbusservice.add_path("/ProductName", productname)
         self._dbusservice.add_path("/CustomName", customname)
-        self._dbusservice.add_path("/FirmwareVersion", "0.1.4 (20231024)")
+        self._dbusservice.add_path("/FirmwareVersion", "0.1.5 (20231218)")
         # self._dbusservice.add_path('/HardwareVersion', '')
         self._dbusservice.add_path("/Connected", 1)
 
@@ -605,6 +605,15 @@ def main():
             logging.warning(
                 "Waiting since %s seconds for receiving first data..." % str(i * 5)
             )
+
+        # check if timeout was exceeded
+        if timeout <= (i * 5):
+            logging.error(
+                "Driver stopped. Timeout of %i seconds exceeded, since no new MQTT message was received in this time."
+                % timeout
+            )
+            sys.exit()
+
         sleep(5)
         i += 1
 
